e6246b65ad7d5cdd3ed4182661bbe4d1efecf04b
kwon <bokyung_kwon@metaps.com>
2019-09-06 21:06:40 +0900

    Initial Commit

---
+++ GitHubUser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubUser
+//
+//  Created by kwon bokyung on 2019/09/06.
+//  Copyright © 2019 Ellie Kwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
